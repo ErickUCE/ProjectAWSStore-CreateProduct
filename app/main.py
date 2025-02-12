f3f3dc38e3b6c0977365e2718ce36025703165e6
erickuce1509 <erickuce1509@gmail.com>
2025-02-12 01:33:45 -0500

    feat:

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routes import router
 from dotenv import load_dotenv
 import os
+from fastapi.middleware.cors import CORSMiddleware  # ✅ Importar CORSMiddleware
+
 
 load_dotenv()  # 🔥 Esto carga las variables de `.env`
 
@@ -9,6 +11,14 @@ load_dotenv()  # 🔥 Esto carga las variables de `.env`
 print("📌 PROVIDER_SERVICE_URL:", os.getenv("PROVIDER_SERVICE_URL"))
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir solicitudes desde el frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 # Incluir las rutas
 app.include_router(router)
