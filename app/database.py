0296a9a20ff9ecc43238f394ead03b3e94f15fe5
erickuce1509 <erickuce1509@gmail.com>
2025-02-14 04:57:30 -0500

    CI/CD para auth: Changes ips to elastics

--- app/database.py
+++ app/database.py
@@ -11,9 +11,9 @@ DB_USER = os.getenv("DB_USER", "default_user")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "default_password")  # ✅ Evitar None
 DB_PASSWORD = urllib.parse.quote_plus(DB_PASSWORD)  # ✅ Codificar contraseña
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME", "testdb")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 # ✅ Construir la URL de conexión a MySQL
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
